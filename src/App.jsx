43a2f6eee3b6323d2ff4f2b968281f329dbf1229
ibrahim alsmairi <54236016+ibrahimALS@users.noreply.github.com>
2023-04-20 22:13:11 +0300

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import { Button } from '@mui/material'
+import './App.css'
+function App() {
+  return (
+    <div className="App">
+      <Button color='primary'>Hello world</Button>
+    </div>
+  )
+}
+
+export default App
