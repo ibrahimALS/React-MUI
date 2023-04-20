2587c99cc5f8173f702c2fd97ab10f54713d3e41
ibrahim alsmairi <54236016+ibrahimALS@users.noreply.github.com>
2023-04-20 22:19:38 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
-import { Button } from '@mui/material'
+import { Button, Typography } from '@mui/material'
+
 import './App.css'
 function App() {
   return (
     <div className="App">
-      <Button color='primary'>Hello world</Button>
+      <Typography variant='h1'>Mui React</Typography>
+      <Button variant='contained'>Button</Button>
+      <Button variant='contained' color='success'>Button</Button>
+      <Button variant='contained' color='error'>Button</Button>
     </div>
   )
 }
